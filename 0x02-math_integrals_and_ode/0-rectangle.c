f8bfe2e7f077dbbde172b847519b0970fdeabaf7
mrgiulls <giuliano.mesias@hotmail.com>
2021-08-22 19:26:06 -0500

    Area of rectangle in progress

---
+++ 0x02-math_integrals_and_ode/0-rectangle.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include "rectangle.h"
+#include <stdio.h>
+
+double rectangle_method(double a,double b, int step)
+{
+double rectangle_integral;
+
+    step = 2 * (a + b);
+    rectangle_integral = a * b;
+
+    
+return 0 ;
+}
---
+++ 0x02-math_integrals_and_ode/rectangle.h
@@ -0,0 +1,9 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <stdarg.h>
+#include <math.h>
+
+double rectangle_method(double a, double b, int step);
+
+#endif
